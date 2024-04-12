4088c8f409e4a85ac91786ddca95e359571f87a1
Mit Vasani <vasanimit9@gmail.com>
2024-04-12 15:14:08 +0530

    fix module resolve issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite';
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: {
+      react: "@preact/compat",
+      "react-dom": "@preact/compat",
+    },
+  },
   build: {
     rollupOptions: {
-      input: './src/main.tsx',
+      input: "./src/main.tsx",
       output: {
-        entryFileNames: '[name].js'
-      }
-    }
-  }
-})
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
